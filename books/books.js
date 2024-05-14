e813469ce13387d9f35dc9e5435d2344b2c8099d
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-05-14 20:45:00 +0530

    added mongodb

--- books/books.js
+++ books/books.js
@@ -2,6 +2,21 @@
 const express = require("express");
 const app = express();
 
+//loading all env variables
+require("dotenv").config();
+
+//Load mongoose
+const mongoose = require("mongoose");
+
+//connect to db
+const MONGODB_URL = process.env.MONGODB_URL;
+try {
+  mongoose.connect(MONGODB_URL);
+  console.log("Connected to db (bookservice)");
+} catch (error) {
+  console.log("error: ", error);
+}
+
 app.get("/", (req, res) => {
   res.send("This is our main endpoint!");
 });
